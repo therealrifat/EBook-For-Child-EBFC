999e33d3dfd484596cd907f9963e8d3730601de3
Rifat islam <merifatislam@gmail.com>
2026-07-20 15:20:54 +0600

    update js file

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let imgdisplay =document.getElementById('imgId')
+let engData =document.getElementById('engName')
+let rmzData =document.getElementById('rmName')
+let bnData =document.getElementById('bnName')
 async function getdata(){
     let response = await fetch("bird.json");
     let json = await response.json()
-    console.log(json);
+    // console.log(json);
+    const dataProces = json[0];
+    imgdisplay.src=dataProces.img
+    engData.innerText= dataProces.name;
+    rmzData.innerText= dataProces.bnRomanize;
+    bnData.innerText= dataProces.bangla;
+    console.log(dataProces.name);
+
     
 }
 getdata()
