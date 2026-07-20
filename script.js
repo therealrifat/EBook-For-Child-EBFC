ab24baa71d159e2f203541908f1c9967263006fa
Rifat islam <merifatislam@gmail.com>
2026-07-20 14:21:07 +0600

    add and move some file

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// async function (params) {
+async function getdata(){
+    let response = await fetch("bird.json");
+    let json = await response.json()
+    console.log(json);
     
-// }
-
-function test(){
-    console.log('ok mamu');
 }
-test()
+getdata()
