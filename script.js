6b2391cc1fdc3471aacfc5457dbb1337cb72814b
Rifat islam <merifatislam@gmail.com>
2026-07-20 17:54:52 +0600

    add DOM

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let imgdisplay =document.getElementById('imgId')
-let engData =document.getElementById('engName')
-let rmzData =document.getElementById('rmName')
-let bnData =document.getElementById('bnName')
-let div = document.querySelector('.main-div')
+let parentDiv =document.querySelector('.container')
+
 async function getdata(){
     let response = await fetch("bird.json");
     let jsonData = await response.json()
     // console.log(json);
     const dataProces = jsonData[0];
-    imgdisplay.src=dataProces.img
-    engData.innerText= dataProces.name;
-    rmzData.innerText= dataProces.bnRomanize;
-    bnData.innerText= dataProces.bangla;
+    
     
     for (const arrydata of jsonData) {
-        console.log(arrydata.name);
-        let pTag = document.createElement('p')
-        pTag.innerText= `${arrydata.name} ${arrydata.bnRomanize} ${arrydata.bangla} `
         
-        
-        
-        
-        div.appendChild(pTag)
+       let cardDiv =document.createElement('div');
+       let img = document.createElement('img')
+       img.src ='./Image file/birds-Img/magpie.jpg'
+       img.width ="200"
+       let pTag = document.createElement('p')
+       pTag.innerText= `${arrydata.name} ${arrydata.bnRomanize} ${arrydata.bangla} `
+       
+       
+       
+       
+       parentDiv.appendChild(cardDiv)
+       cardDiv.appendChild(img)
+       cardDiv.appendChild(pTag)
     }
 }
 getdata()
